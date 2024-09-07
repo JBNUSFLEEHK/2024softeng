a95fcc14ce4ef5bf9301e6aa6e1a56478ed0ba42
JBNUSFLEEHK <93865533+JBNUSFLEEHK@users.noreply.github.com>
2024-09-07 22:54:33 +0900

    Update and rename temp_f2c to temp_f2c.py

---
+++ hw02/temp_f2c.py
@@ -0,0 +1,17 @@
+def f2c(temp_f: float) -> float: 
+    return (temp_f-32)*5/9
+
+
+def main():
+    temp_f = 80
+    temp_c = f2c(temp_f)
+    print(f"{temp_f}F => {temp_c:.1f}C")
+
+
+    pass               
+
+if __name__ == '__main__':
+    main()
+
+
+    
